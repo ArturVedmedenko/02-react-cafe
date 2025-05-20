7a971d0f1f9f9276ef433404002d537e4251d953
Artur Vedmedenko <vedmedenko.a@gmail.com>
2025-05-20 22:18:52 +0300

    Add componets folders and files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-// src/main.tsx
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
+import 'modern-normalize';
+import './index.css';
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
